469cea9eddb612589acbab572fab8d13d6d28a33
Radu Ţurcanu <63318406+Rainbovv@users.noreply.github.com>
2020-04-27 04:02:51 +0300

    Update Aplication.java

--- src/Aplication.java
+++ src/Aplication.java
@@ -3,26 +3,19 @@ public class Aplication {
 
 	public static void main(String[] args) {
 		
-		Educated educated = new Educated("Vasile Pupkin",
-									      17, 9.52f, 9);
+		Educated educated = new Educated("Vasile Pupkin", 17, 9.52f, 9);
 		
 		educated.printInfo();
 	
-		Pupil pupil = new Pupil("Alexei Petrovici", 
-							    "Liceul Teoretic \"Vasea Borş\"",
-							     18, 8.72f, 12);
+		Pupil pupil = new Pupil("Alexei Petrovici", "Liceul Teoretic \"Vasea Borş\"", 18, 8.72f, 12);
 		
 		pupil.printInfo();
 		
-		Student student = new Student("Ion Brânză", "UTM", 
-								      "Facultatea de prăşit",
-								       26, 8.72f, 6);
+		Student student = new Student("Ion Brânză", "UTM", "Facultatea de prăşit", 26, 8.72f, 6);
 		
 		student.printInfo();
 		
-		Master master = new Master("Vitalie Chişlămău", "USM",
-								   "Facultatea de tuns bursuci",
-								    30, 8.15f, 3);
+		Master master = new Master("Vitalie Chişlămău", "USM", "Facultatea de tuns bursuci", 30, 8.15f, 3);
 		
 		master.printInfo();
 		
